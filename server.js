b2c01e87c70a570dc9360f319b0938d4f8bc1d29
Gourab dey <116297847+codinggourab@users.noreply.github.com>
2026-03-26 18:04:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ app.set('io', io);
 
 // middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // routes
 app.use('/api/auth', authRoutes);
